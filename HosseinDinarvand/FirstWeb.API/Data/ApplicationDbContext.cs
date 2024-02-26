3b962579c892bf40c22765ce2f43724e7cc86830
HosseinDinarvnad <h.dinarvand@outlook.com>
2024-02-26 18:16:58 +0330

    move-project-to-folder

---
+++ HosseinDinarvand/FirstWeb.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using FirstWeb.API.Model.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWeb.API.Data
+{
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-70F4127;Database=WebAPIDb;Trusted_Connection=True;TrustServerCertificate=True");
+            }
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ HosseinDinarvand/FirstWeb.API/Model/DTO/ProductDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstWeb.API.Model.DTO
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Name has to be a minimum of 5 characters")]
+        [MaxLength(15, ErrorMessage = "Name has to be a maximum of 15 characters")]
+        public string Name { get; set; } = String.Empty;
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Name has to be a minimum of 5 characters")]
+        [MaxLength(10, ErrorMessage = "Name has to be a maximum of 10 characters")]
+        public string Category { get; set; } = String.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}

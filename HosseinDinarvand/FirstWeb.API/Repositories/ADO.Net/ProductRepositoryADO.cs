c234adf2fac94c3fe76761a0fa70b66113953619
HosseinDinarvand <131610708+HosseinDinarvand@users.noreply.github.com>
2024-02-27 22:43:56 +0330

    Implementations of the week 5 (#2)
    * add .gitignore file
    
    * code changes applied
    
    * impelement In-Memory Caching
    
    * implement output caching
    
    * implement response caching
    
    * implement rate limiting
    
    * implement response compression
    
    * update .net 7 to .net 8
    
    * update all packages to last version
    
    * change .gitignore file
    
    * JWT-authentication
    
    * handle cross-origin resurce sharing
    
    * add-IP safelist-middleware
    
    * edit-and-update-code
    
    * move-project-to-folder
    
    * delete-already-code

---
+++ HosseinDinarvand/FirstWeb.API/Repositories/ADO.Net/ProductRepositoryADO.cs
@@ -0,0 +1,32 @@
+﻿using FirstWeb.API.Model.Domain;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace FirstWeb.API.Repositories.ADO.Net
+{
+    public class ProductRepositoryADO : IProductRepositoryADO
+    {
+        private readonly IConfiguration configuration;
+        public ProductRepositoryADO(IConfiguration _configuration)
+        {
+            this.configuration = _configuration;
+        }
+        public Product? getByName(string name)
+        {
+            Product product = new Product();
+            SqlConnection connection = new SqlConnection(configuration.GetConnectionString("DefaultConnectionString"));
+            SqlCommand command = new SqlCommand($"SELECT * FROM Products WHERE Name = N'{name}'", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+
+            dataAdapter.Fill(dataTable);
+
+            product.Id = int.Parse(dataTable.Rows[0]["Id"].ToString());
+            product.Name = dataTable.Rows[0]["Name"].ToString();
+            product.Category = dataTable.Rows[0]["Category"].ToString();
+            product.Price = decimal.Parse(dataTable.Rows[0]["Price"].ToString());
+
+            return product;
+        }
+    }
+}
---
+++ HosseinDinarvand/FirstWeb.API/Services/In-Memory Caching/CacheServiceInMemory.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Runtime.Caching;
+
+namespace FirstWeb.API.Services.In_Memory_Caching
+{
+    public class CacheServiceInMemory : ICacheServiceInMemory
+    {
+        private ObjectCache _memoryCache = MemoryCache.Default;
+        public T getData<T>(string key)
+        {
+            try
+            {
+                T item = (T)_memoryCache.Get(key);
+                return item;
+            }
+
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public object removeData(string key)
+        {
+            var result = true;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(key))
+                    _memoryCache.Remove(key);
+                else
+                    result = false;
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public bool setData<T>(string key, T data, DateTimeOffset expirationTime)
+        {
+            bool result = true;
+            try
+            {
+                if (!string.IsNullOrEmpty(key))
+                    _memoryCache.Set(key, data, expirationTime);
+                else
+                    result = false;
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}

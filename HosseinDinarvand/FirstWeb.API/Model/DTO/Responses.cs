5e2a119d0dff9804298ad787ed895225496ae82d
MahshadChabok <81689318+mahshadch1381@users.noreply.github.com>
2024-03-02 23:40:39 +0330

    Delete merging

--- HosseinDinarvand/FirstWeb.API/Model/DTO/Responses.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FirstWeb.API.Model.DTO
-{
-    public class Responses
-    {
-        public record class GeneralResponse(bool flag,string Message);
-        public record class LoginResponse(bool flag,string Token,string Message);
-    }
-}
--- HosseinDinarvand/FirstWeb.API/Services/Distributed Caching/ICacheServiceDistributed.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FirstWeb.API.Services
-{
-    public interface ICacheServiceDistributed
-    {
-        T getData<T>(string key);
-        bool setData<T>(string key, T data,DateTimeOffset expirationTime);
-        object removeData(string key);
-    }
-}

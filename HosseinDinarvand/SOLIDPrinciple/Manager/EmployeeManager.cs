3dc6789e38d15806d145bbe584584836318d96b2
HosseinDinarvnad <h.dinarvand@outlook.com>
2024-03-03 10:11:23 +0330

    single-responsibility-principle

---
+++ HosseinDinarvand/SOLIDPrinciple/Manager/EmployeeManager.cs
@@ -0,0 +1,21 @@
+﻿using SOLIDPrinciple.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciple.Manager
+{
+    public class EmployeeManager
+    {
+        private List<Employee> employees = new List<Employee>();
+
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+            Console.WriteLine("Employee addeds successfuly.");
+        }
+
+    }
+}
---
+++ HosseinDinarvand/SOLIDPrinciple/Program.cs
@@ -0,0 +1,15 @@
+﻿using SOLIDPrinciple.Manager;
+using SOLIDPrinciple.Model;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        EmployeeManager manager = new EmployeeManager();
+
+        manager.AddEmployee(new Employee { Id = 1, Name = "John" });
+        manager.AddEmployee(new Employee { Id = 2, Name = "Jack" });
+
+        Console.ReadKey();
+    }
+}
